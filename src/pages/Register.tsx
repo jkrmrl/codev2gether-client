5579617cce9ccd04ef11937d18e6d06810bc31be
jkrmrl <julromeral@gmail.com>
2025-02-20 16:53:23 +0800

    feat(register): add header and form to register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../components/Form';
+import Header from '../components/Header';
+
+const Register: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Form />
+        </>
+    )
+}
+
+export default Register;
