8aa7954133cd8d4eaf2b53aa69339d548cae353b
jkrmrl <julromeral@gmail.com>
2025-02-20 16:46:24 +0800

    feat(login): add header and form to login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../components/Form';
+import Header from '../components/Header';
+
+const Login: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Form />
+        </>
+    )
+}
+
+export default Login;
